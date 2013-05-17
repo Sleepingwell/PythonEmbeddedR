0f3ab91eeef211591f17fc8389adddabfaeaca8c
Simon Knapp <simon.knapp@verusol.com>
2013-05-18 00:05:38 +1000

    First commit of code.

---
+++ PythonEmbeddedR.cpp
@@ -0,0 +1,62 @@
+#include <Python.h>
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Parse.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+    static PyObject *RRunnerError;
+
+    static PyObject* _run_R(PyObject *self, PyObject *args) {
+        char* code;
+        SEXP e, tmp, rRes;
+        PyObject* pyRes;
+        R_len_t i;
+        int hasError;
+        ParseStatus status;
+        if (!PyArg_ParseTuple(args, "s", &code)) return NULL;
+        PROTECT(tmp = mkString(code));
+        PROTECT(e = R_ParseVector(tmp, -1, &status, R_NilValue));
+        if(status != PARSE_OK) {
+            PyErr_SetString(RRunnerError, "Error when parsing R code");
+            return NULL;
+        } else {
+            for(i=0; i<length(e); i++) {
+                rRes = R_tryEval(VECTOR_ELT(e,i), R_GlobalEnv, &hasError);
+                if(hasError) break;
+            }
+            if(hasError) {
+                PyErr_SetString(RRunnerError, "Error when executing R code");
+                return NULL;
+            } else if(isString(rRes)) {
+                pyRes = Py_BuildValue("s", CHAR(STRING_ELT(rRes,0)));
+            } else if(isReal(rRes)) {
+                pyRes = PyTuple_New(length(rRes));
+                for(Py_ssize_t rl=0; rl<length(rRes); ++rl) PyTuple_SetItem(pyRes, rl, PyFloat_FromDouble(REAL(rRes)[i]));
+            } else {
+                pyRes = Py_BuildValue("s", "Cannot handle return type");
+                Py_INCREF(pyRes);
+            }
+        }
+        UNPROTECT(2);
+        return pyRes;
+    }
+
+    static PyMethodDef EmbeddedRMethods[] = {
+        {"run_R", _run_R, METH_VARARGS|METH_KEYWORDS, "Run Some R code."},
+        {NULL, NULL, 0, NULL}
+    };
+
+    PyMODINIT_FUNC initPythonEmbeddedR(void) {
+        PyObject *m;
+        m = Py_InitModule("PythonEmbeddedR", EmbeddedRMethods);
+        if (m == NULL) return;
+        RRunnerError = PyErr_NewException("runr.error", NULL, NULL);
+        Py_INCREF(RRunnerError);
+        PyModule_AddObject(m, "error", RRunnerError);
+    }
+#ifdef __cplusplus
+} // end extern "C"
+#endif // __cplusplus
---
+++ README.md
@@ -0,0 +1,20 @@
+This is a very simple example of how to embed R in Python. Other mechanisms are out there to do this (e.g. [Rpy](http://rpy.sourceforge.net/) and [pyRserve](https://pypi.python.org/pypi/pyRserve/)) but this might be useful if you have simple requirements and want to limit the number of dependencies you have.
+
+Remember that if you are using this on a server, R is not reentrant, so this may be racy if you don't set it up right.
+
+
+## Introduction
+
+The function defined here evaluates an arbitrary set of R expressions, and will return the result of the last if it is a string or vector of reals. Extending this to other types would be straight forward (though perhaps fiddly), but at the time of writing this was just a demonstration project.
+
+## Files
+- init.h, init.cpp: Funcitons to setup and tear down R,
+- library_attach.cpp: Calls the functions defined in init.h when the library is attached/detached,
+- PythonEmbeddedR.cpp: Defines the one function of this package, and
+- init.cpp: Defines the functions declared in init.h.
+
+## Compiling
+
+On windows, you can just use the solution provided, after modifying the include and library paths (via project->properties) to suite your environment.
+
+If your using GCC or any other compiler, then you can probably figure it out yourself (... and I mean that respectfully), though if you need a hand, please ask and I will document it.
